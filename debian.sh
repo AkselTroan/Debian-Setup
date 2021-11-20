ba3fe9f98ec44e5d916f3305d46692613b9c6780
Aksel Troan <aksel_troan_98@hotmail.com>
2021-11-21 00:47:49 +0100

    Added sudo function

--- debian.sh
+++ debian.sh
@@ -19,11 +19,31 @@ install_packages(){
 	while read -r line; do apt-get install $line; done < ./Resources/package_list.txt
 }
 
+sets_up_sudo(){
+	
+	echo ""
+	echo "Setting up PATH"
+	echo ""
+
+	export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/usr/sbin"
+	
+	apt-get install sudo
+	
+	# Here we can insert a custom user. Input prompt.
+	
+	usermod -aG sudo troan
+
+}
+
+
 install_and_setup_i3_gaps(){
-    which git
-    if [$? -neq 0] then;
+    
+	which git
+    
+	if [$? -neq 0] then;
         exit
-    else
+    
+	else
         git clone https://github.com/maestrogerardo/i3-gaps-deb.git
         cd i3-gaps-deb
         sh i3-gaps-deb
@@ -44,12 +64,15 @@ options(){
 read -p "$newline$arrow" option
 case "$option" in  # For each option given, a function need to be added.
 	1 ) echo "$CYAN Installing Essential packages... $ENDCOLOR"
+		sets_up_sudo
 		install_packages
 		;;
 	2 ) echo "$CYAN Installing and setting up i3-gaps... $ENDCOLOR"
+		sets_up_sudo
 		install_and_setup_i3_gaps
 		;;
 	3 ) echo "$CYAN Installing essential packages and setting up i3-gaps... $ENDCOLOR"
+		sets_up_sudo
 		install_packages
         install_and_setup_i3_gaps
 		;;
